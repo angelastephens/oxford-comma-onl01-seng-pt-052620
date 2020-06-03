ed9984f296a8ccf701999bdfff38283b9d8722b8
Angela Stephens <angelastephens317@gmail.com>
2020-06-03 03:06:11 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,7 +7,7 @@ array.join(" and ")
 end
 
 def oxford_comma(array)
-    if array.length == 3
+    if array.length == 2
         array.join(" and ")
     elsif array.length > 2
         array.insert(-2, "and")
