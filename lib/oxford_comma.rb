250ba1381a2e6b215310b73b61c669db7cf6f50a
Angela Stephens <angelastephens317@gmail.com>
2020-06-03 03:14:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,13 @@
 def oxford_comma(array)
+array.join
+end
+
+def oxford_comma(array)
+array.join(" and ") 
+end
+
+
+def oxford_comma(array)
   return array.join(' and ') if array.size < 3
   array[-1] = "and " + array[-1]
   array.join(', ')
